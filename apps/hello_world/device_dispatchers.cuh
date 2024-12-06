185c72ad57cb3f0228d9ac1afd4ad4f6e892498e
Yanwen Xu <xuyanwen2012@gmail.com>
2024-12-05 19:23:32 -0800

    rm

--- apps/hello_world/device_dispatchers.cuh
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "app_data.hpp"
-
-namespace cuda {
-
-void run_stage1(Engine &engine, const cuda::AppData &app_data);
-
-}  // namespace cuda
--- redwood/utils.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <spdlog/spdlog.h>
-
-#include <source_location>
-
-#define SPD_TRACE_FUNC                                                         \
-  spdlog::trace(std::source_location::current().function_name());
