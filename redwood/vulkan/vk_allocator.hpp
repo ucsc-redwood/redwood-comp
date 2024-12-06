eaadd5fde8a3c10023e4371642bfc521ba65333b
Yanwen Xu <xuyanwen2012@gmail.com>
2024-12-06 11:16:51 -0800

    cp resource

--- redwood/vulkan/vk_allocator.hpp
+++ redwood/vulkan/vk_allocator.hpp
@@ -5,38 +5,9 @@
 
 namespace vulkan {
 
-// class VulkanMemoryResource : public std::pmr::memory_resource {
-//  public:
-//   explicit VulkanMemoryResource(Engine& eng) : engine_(eng) {}
-
-//  protected:
-//   void* do_allocate(std::size_t bytes, std::size_t) override {
-//     if (allocated_) {
-//       throw std::bad_alloc();
-//     }
-
-//     buffer_ = engine_.buffer(bytes);
-
-//     allocated_ = true;
-//     return buffer_->as<void*>();
-//   }
-
-//   void do_deallocate(void*, std::size_t, std::size_t) override {
-//     // NO OP
-//   }
-
-//   bool do_is_equal(const memory_resource& other) const noexcept override {
-//     // Equality means same engine instance
-//     auto* o = dynamic_cast<const VulkanMemoryResource*>(&other);
-//     return o && (&o->engine_ == &this->engine_);
-//   }
-
-//  private:
-//   Engine& engine_;
-//   std::shared_ptr<Buffer> buffer_;
-
-//   bool allocated_ = false;
-// };
+// --------------------------------------------------------------------------
+// VulkanMemoryResource for AppData
+// --------------------------------------------------------------------------
 
 class VulkanMemoryResource : public std::pmr::memory_resource {
  public:
