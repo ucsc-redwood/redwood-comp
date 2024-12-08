c9719da9c1005a2c71bc89e6943c8342566eb2e0
Yanwen Xu <xuyanwen2012@gmail.com>
2024-12-07 20:03:26 -0800

    bm vulkan cifar dense working

--- apps/cifar/dense/xmake.lua
+++ apps/cifar/dense/xmake.lua
@@ -6,6 +6,9 @@ target("app-cifar-dense")
     add_files("main.cpp", "host/*.cpp", "app_data.cpp")
     add_packages("spdlog", "cli11", "yaml-cpp")
     add_deps("cpu-backend")
+    if is_plat("android") then
+      on_run(run_on_android)
+    end
 
     -- CUDA related (optional)
     if has_config("cuda-backend") then
@@ -16,54 +19,47 @@ target("app-cifar-dense")
       add_cugencodes("native")
     end
 
-    -- Android related (optional)
+    -- Vulkan related (optional)
     if has_config("vulkan-backend") then
       add_defines("REDWOOD_VULKAN_BACKEND")
       add_headerfiles("vulkan/*.hpp")
-      add_files("vulkan/*.cpp")
-
-      if is_plat("android") then
-        on_run(run_on_android)
-      end
-      
+      add_files("vulkan/*.cpp")      
       add_deps("vk-backend")
       add_packages("vulkan-hpp", "vulkan-memory-allocator")
     end
 
-target("bm-cifar-dense")
+target("bm-cifar-dense-cpu")
     set_kind("binary")
     add_includedirs("$(projectdir)")
     add_headerfiles("*.hpp", "host/*.hpp")
     add_files("bm_pinned.cpp", "host/*.cpp", "app_data.cpp")
     add_packages("spdlog", "cli11", "yaml-cpp")
     add_deps("cpu-backend")
+    if is_plat("android") then
+      on_run(run_on_android)
+    end
 
     add_packages("benchmark")
 
-    -- CUDA related (optional)
-    if has_config("cuda-backend") then
-      add_defines("REDWOOD_CUDA_BACKEND")
-      add_deps("cu-backend")
-      add_headerfiles("cuda/*.cuh")
-      add_files("cuda/*.cu")
-      add_cugencodes("native")
+target("bm-cifar-dense-vk")
+    set_kind("binary")
+    add_includedirs("$(projectdir)")
+    add_headerfiles("*.hpp")
+    add_files("bm_vulkan.cpp", "app_data.cpp")
+    add_packages("spdlog", "cli11", "yaml-cpp")
+
+    if is_plat("android") then
+      on_run(run_on_android)
     end
 
-    -- Android related (optional)
+    add_packages("benchmark")
+
+    -- Vulkan related (optional)
     if has_config("vulkan-backend") then
       add_defines("REDWOOD_VULKAN_BACKEND")
       add_headerfiles("vulkan/*.hpp")
-      add_files("vulkan/*.cpp")
-
-      if is_plat("android") then
-        on_run(run_on_android)
-      end
-      
+      add_files("vulkan/*.cpp")      
       add_deps("vk-backend")
       add_packages("vulkan-hpp", "vulkan-memory-allocator")
     end
-
-    
-
-
 
