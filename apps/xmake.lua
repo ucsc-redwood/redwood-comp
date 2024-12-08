6e5f2a90904f3d42ee58eed6e6345f7517f9ac70
Yanwen Xu <xuyanwen2012@gmail.com>
2024-12-07 19:44:43 -0800

    save

--- apps/xmake.lua
+++ apps/xmake.lua
@@ -1,3 +1,26 @@
+if is_plat("android") then
+    package("benchmark")
+        set_kind("library")
+        add_deps("cmake")
+        set_urls("https://github.com/google/benchmark.git")
+        add_versions("v1.9.0", "12235e24652fc7f809373e7c11a5f73c5763fc4c")
+        
+        -- Add description and homepage for better package management
+        set_description("A microbenchmark support library")
+        set_homepage("https://github.com/google/benchmark")
+
+        on_install(function(package)
+            local configs = {
+                "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"),
+                "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"),
+                "-DBENCHMARK_DOWNLOAD_DEPENDENCIES=on",
+                "-DHAVE_THREAD_SAFETY_ATTRIBUTES=0"
+            }
+            import("package.tools.cmake").install(package, configs)
+        end)
+    package_end()
+end
+
 add_requires("yaml-cpp")
 add_requires("benchmark")
 add_requires("cli11")
