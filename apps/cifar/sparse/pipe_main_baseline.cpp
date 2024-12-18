caedffdc93edb2d8ed345acfe1bad3de90298121
Yanwen Xu <xuyanwen2012@gmail.com>
2024-12-18 12:31:53 -0800

    s

---
+++ apps/cifar/sparse/pipe_main_baseline.cpp
@@ -0,0 +1,154 @@
+#include "../../app.hpp"
+#include "app_data.hpp"
+#include "host/host_dispatcher.hpp"
+#include "redwood/backends.hpp"
+
+constexpr auto n_iterations = 10000;
+
+void run_cuda_only();
+void run_vulkan_only();
+
+void run_cpu_unpinned() {
+  auto mr = std::pmr::new_delete_resource();
+  AppData app_data(mr);
+
+  const auto n_threads = std::thread::hardware_concurrency();
+  core::thread_pool unpinned_pool(n_threads);
+
+  auto start = std::chrono::high_resolution_clock::now();
+
+  std::cout << "Running " << n_iterations << " iterations" << std::endl;
+  for (size_t i = 0; i < n_iterations; ++i) {
+    cpu::kernels::sparse::run_stage1(app_data, unpinned_pool, n_threads, true);
+    cpu::kernels::sparse::run_stage2(app_data, unpinned_pool, n_threads, true);
+    cpu::kernels::sparse::run_stage3(app_data, unpinned_pool, n_threads, true);
+    cpu::kernels::sparse::run_stage4(app_data, unpinned_pool, n_threads, true);
+    cpu::kernels::sparse::run_stage5(app_data, unpinned_pool, n_threads, true);
+    cpu::kernels::sparse::run_stage6(app_data, unpinned_pool, n_threads, true);
+    cpu::kernels::sparse::run_stage7(app_data, unpinned_pool, n_threads, true);
+    cpu::kernels::sparse::run_stage8(app_data, unpinned_pool, n_threads, true);
+    cpu::kernels::sparse::run_stage9(app_data, unpinned_pool, n_threads, true);
+
+    if (i % 100 == 0) {
+      std::cout << "." << std::flush;
+    }
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+  auto duration =
+      std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+  std::cout << "\nTotal time: " << duration.count() << "ms" << std::endl;
+  std::cout << "Average time per iteration: "
+            << static_cast<float>(duration.count()) / n_iterations << "ms"
+            << std::endl;
+  std::cout << "Throughput: "
+            << static_cast<float>(n_iterations) * 1000 / duration.count()
+            << " iterations/s" << std::endl;
+}
+
+#ifdef REDWOOD_CUDA_BACKEND
+
+#include "cuda/cu_dispatcher.cuh"
+#include "redwood/cuda/cu_mem_resource.cuh"
+
+void run_cuda_only() {
+  cuda::CudaMemoryResource mr;
+  AppData app_data(&mr);
+
+  cuda::Dispatcher dispatcher(app_data, 1);
+
+  auto start = std::chrono::high_resolution_clock::now();
+
+  std::cout << "Running " << n_iterations << " iterations" << std::endl;
+  for (size_t i = 0; i < n_iterations; ++i) {
+    dispatcher.run_stage1(0, true);
+    dispatcher.run_stage2(0, true);
+    dispatcher.run_stage3(0, true);
+    dispatcher.run_stage4(0, true);
+    dispatcher.run_stage5(0, true);
+    dispatcher.run_stage6(0, true);
+    dispatcher.run_stage7(0, true);
+    dispatcher.run_stage8(0, true);
+    dispatcher.run_stage9(0, true);
+
+    if (i % 100 == 0) {
+      std::cout << "." << std::flush;
+    }
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+  auto duration =
+      std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+  std::cout << "\nTotal time: " << duration.count() << "ms" << std::endl;
+  std::cout << "Average time per iteration: "
+            << static_cast<float>(duration.count()) / n_iterations << "ms"
+            << std::endl;
+  std::cout << "Throughput: "
+            << static_cast<float>(n_iterations) * 1000 / duration.count()
+            << " iterations/s" << std::endl;
+}
+
+#endif
+
+#ifdef REDWOOD_VULKAN_BACKEND
+
+#include "vulkan/vk_dispatchers.hpp"
+
+void run_vulkan_only() {
+  vulkan::Engine engine;
+  AppData app_data(engine.get_mr());
+
+  vulkan::Dispatcher dispatcher(engine, app_data);
+
+  auto seq = engine.sequence();
+
+  auto start = std::chrono::high_resolution_clock::now();
+
+  std::cout << "Running " << n_iterations << " iterations" << std::endl;
+  for (size_t i = 0; i < n_iterations; ++i) {
+    dispatcher.run_stage1(seq.get(), true);
+    dispatcher.run_stage2(seq.get(), true);
+    dispatcher.run_stage3(seq.get(), true);
+    dispatcher.run_stage4(seq.get(), true);
+    dispatcher.run_stage5(seq.get(), true);
+    dispatcher.run_stage6(seq.get(), true);
+    dispatcher.run_stage7(seq.get(), true);
+    dispatcher.run_stage8(seq.get(), true);
+    dispatcher.run_stage9(seq.get(), true);
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+  auto duration =
+      std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+  std::cout << "\nTotal time: " << duration.count() << "ms" << std::endl;
+  std::cout << "Average time per iteration: "
+            << static_cast<float>(duration.count()) / n_iterations << "ms"
+            << std::endl;
+  std::cout << "Throughput: "
+            << static_cast<float>(n_iterations) * 1000 / duration.count()
+            << " iterations/s" << std::endl;
+}
+
+#endif
+
+int main(int argc, char **argv) {
+  INIT_APP("pipe-cifar-sparse");
+
+  spdlog::info("Running CPU unpinned");
+  run_cpu_unpinned();
+
+  if constexpr (is_backend_enabled(BackendType::kCUDA)) {
+    spdlog::info("Running CUDA only");
+    run_cuda_only();
+  }
+
+  if constexpr (is_backend_enabled(BackendType::kVulkan)) {
+    spdlog::info("Running Vulkan only");
+    run_vulkan_only();
+  }
+
+  return EXIT_SUCCESS;
+}
--- apps/cifar/sparse/xmake.lua
+++ apps/cifar/sparse/xmake.lua
@@ -120,14 +120,32 @@ end
 -- Pipe Benchmark
 -- ----------------------------------------------------------------------------
 
-target("pipe-cifar-sparse")
+target("pipe-cifar-sparse-baseline")
     set_kind("binary")
     add_includedirs("$(projectdir)")
     add_headerfiles(cpp_header)
-    add_files("pipe_main_cpu.cpp", cpp_source)
+    add_files("pipe_main_baseline.cpp", cpp_source)
     add_packages("spdlog", "cli11", "yaml-cpp")
     add_deps("cpu-backend")
     if is_plat("android") then
       on_run(run_on_android)
+    end
+
+    -- CUDA related (optional)
+    if has_config("cuda-backend") then
+      add_defines("REDWOOD_CUDA_BACKEND")
+      add_deps("cu-backend")
+      add_headerfiles("cuda/*.cuh")
+      add_files("cuda/*.cu")
+      add_cugencodes("native")
+    end
+
+    -- Vulkan related (optional)
+    if has_config("vulkan-backend") then
+      add_defines("REDWOOD_VULKAN_BACKEND")
+      add_headerfiles("vulkan/*.hpp")
+      add_files("vulkan/*.cpp")      
+      add_deps("vk-backend")
+      add_packages("vulkan-hpp", "vulkan-memory-allocator")
     end
 target_end()
