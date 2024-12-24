3adde02e72610a6e22c5704bfc4b30a897de0002
Yanwen Xu <xuyanwen2012@gmail.com>
2024-12-24 06:42:47 +0000

    add devcontainer

--- xmake.lua
+++ xmake.lua
@@ -1,14 +1,16 @@
 add_rules("mode.debug", "mode.release")
 
 set_languages("c++20")
-if not is_plat("android") then
-    set_toolchains("clang")
-end
+-- if not is_plat("android") then
+--     set_toolchains("clang")
+-- end
 set_warnings("allextra")
 
 -- Global requirements, for all projects
 -- This is very handy library, I recommend it.
 add_requires("spdlog")
+
+-- add_cuflags("-allow-unsupported-compiler", {force = true})
 
 includes("android.lua")
 
